3fbbb5376af4a7aaef4348ce5aab7ed81e42ebe5
mrduman <omer.duman@gmx.de>
2024-04-05 15:10:21 +0200

    bg color changed and react dom implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <>
-      <Button>Insta</Button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   );
 }
