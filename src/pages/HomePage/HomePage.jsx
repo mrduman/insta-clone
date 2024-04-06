3e7f824c1dd57a57c99972079c182307dcde7760
mrduman <omer.duman@gmx.de>
2024-04-06 14:25:54 +0200

    HomePage wip

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,24 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10} border={"1px solid red"}>
+          FeedPosts
+        </Box>
+        <Box
+          flex={3}
+          mr={20}
+          display={{ base: "none", lg: "block" }}
+          maxW={"300px"}
+          border={"1px solid red"}
+        >
+          Suggested Users
+        </Box>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Home;
