67abaf0ec0d05f05c81776ae0871818ce33c1b3e
mrduman <omer.duman@gmx.de>
2024-04-09 16:40:10 +0200

    FeedPost creted

---
+++ src/components/FeedPosts/FeedPost.jsx
@@ -0,0 +1,17 @@
+import { Box, Image } from "@chakra-ui/react";
+import PostHeader from "./PostHeader";
+import PostFooter from "./PostFooter";
+
+const FeedPost = () => {
+  return (
+    <>
+      <PostHeader />
+      <Box my={2}>
+        <Image src="img1.png" alt="user profile pic" />
+      </Box>
+      <PostFooter />
+    </>
+  );
+};
+
+export default FeedPost;
