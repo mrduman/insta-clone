a0dae5b10640689775e7ca29cdac7688a7aecd3f
mrduman <omer.duman@gmx.de>
2024-04-09 21:56:05 +0200

    Suggested User created wip

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,5 +1,37 @@
-const SuggestedUser = () => {
-  return <div>SuggestedUser</div>;
+import { Avatar, Box, Button, Flex, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+
+const SuggestedUser = ({ name, followers, avatar }) => {
+  const [isFollowed, setIsFollowed] = useState(false);
+
+  return (
+    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+      <Flex alignItems={"center"} gap={2}>
+        <Avatar src={avatar} />
+        <VStack spacing={2}>
+          <Box fontSize={12} fontWeight={"bold"}>
+            {name}
+          </Box>
+          <Box fontSize={11} color={"gray.500"} alignSelf={"start"}>
+            {followers} followers
+          </Box>
+        </VStack>
+      </Flex>
+      <Button
+        fontSize={13}
+        bg={"transparent"}
+        p={0}
+        h={"max-content"}
+        fontWeight={"bold"}
+        color={"blue.400"}
+        cursor={"pointer"}
+        _hover={{ color: "white" }}
+        onClick={() => setIsFollowed(!isFollowed)}
+      >
+        {isFollowed ? "Unfollow" : "Follow"}
+      </Button>
+    </Flex>
+  );
 };
 
 export default SuggestedUser;
--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,10 +1,55 @@
-import { VStack } from "@chakra-ui/react";
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
 import SuggestedHeader from "./SuggestedHeader";
-
+import SuggestedUser from "./SuggestedUser";
+import { Link as RouterLink } from "react-router-dom";
 const SuggestedUsers = () => {
   return (
     <VStack py={8} gap={4}>
       <SuggestedHeader />
+
+      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+        <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+          Suggested for you
+        </Text>
+
+        <Text
+          fontSize={12}
+          fontWeight={"bold"}
+          cursor={"pointer"}
+          _hover={{ color: "gray.400" }}
+        >
+          See all
+        </Text>
+      </Flex>
+
+      <SuggestedUser
+        name="Dan Abrahmov"
+        followers={1392}
+        avatar="https://bit.ly/dan-abramov"
+      />
+      <SuggestedUser
+        name="Ryan Florence"
+        followers={666}
+        avatar="https://bit.ly/ryan-florence"
+      />
+      <SuggestedUser
+        name="Christian Nwamba"
+        followers={455}
+        avatar="https://bit.ly/code-beast"
+      />
+
+      <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+        © 2023 Built By{" "}
+        <Link
+          as={RouterLink}
+          to={"https://github.com/mrduman"}
+          target="_blank"
+          color="blue.500"
+          fontSize={14}
+        >
+          Ömer Duman
+        </Link>
+      </Box>
     </VStack>
   );
 };
