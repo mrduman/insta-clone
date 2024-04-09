38903227e4ead82499f4836498600f263530d31f
mrduman <omer.duman@gmx.de>
2024-04-09 16:39:59 +0200

    FeedPosts component created

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,18 @@
 import { Box, Container, Flex } from "@chakra-ui/react";
+import FeedPosts from "../../components/FeedPosts/FeedPosts";
 
 const Home = () => {
   return (
     <Container maxW={"container.lg"}>
       <Flex gap={20}>
-        <Box flex={2} py={10} border={"1px solid red"}>
-          FeedPosts
+        <Box flex={2} py={10}>
+          <FeedPosts />
         </Box>
         <Box
           flex={3}
           mr={20}
           display={{ base: "none", lg: "block" }}
           maxW={"300px"}
-          border={"1px solid red"}
         >
           Suggested Users
         </Box>
