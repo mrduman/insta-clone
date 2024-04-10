f761318f994a66dda443cd7dfe4217acc27151f6
mrduman <omer.duman@gmx.de>
2024-04-10 16:05:08 +0200

    no-issue

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import { Container, Flex, VStack } from "@chakra-ui/react";
+import { Container, Flex } from "@chakra-ui/react";
 import ProfileHeader from "./ProfileHeader";
 import ProfileTabs from "./ProfileTabs";
 import ProfilePosts from "./ProfilePosts";
@@ -15,18 +15,17 @@ const ProfilePage = () => {
         flexDirection={"column"}
       >
         <ProfileHeader />
-
-        <Flex
-          px={{ base: 2, sm: 4 }}
-          maxW={"full"}
-          mx={"auto"}
-          borderTop={"1px solid"}
-          borderColor={"whiteAlpha.300"}
-          direction={"column"}
-        >
-          <ProfilePosts />
-          <ProfileTabs />
-        </Flex>
+      </Flex>
+      <Flex
+        px={{ base: 2, sm: 4 }}
+        maxW={"full"}
+        mx={"auto"}
+        borderTop={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        direction={"column"}
+      >
+        <ProfilePosts />
+        <ProfileTabs />
       </Flex>
     </Container>
   );
