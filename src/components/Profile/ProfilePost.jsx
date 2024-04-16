01f27633ab4bedb9735745d02bd15933ae363149
mrduman <omer.duman@gmx.de>
2024-04-16 19:15:36 +0200

    Modal added

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,41 +1,157 @@
-import { Flex, GridItem, Text } from "@chakra-ui/react";
-import { FaHeart } from "react-icons/fa";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Flex,
+  GridItem,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { FaComment, FaHeart } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import Comment from "../Comment/Comment";
 
-const ProfilePost = () => {
+const ProfilePost = ({ img }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <GridItem
-      overflow={"hidden"}
-      cursor={"pointer"}
-      borderRadius={4}
-      border={"1px solid"}
-      borderColor={"whiteAlpha.300"}
-      position={"relative"}
-      aspectRatio={1 / 1}
-    >
-      <Flex
-        opacity={0}
-        _hover={{ opacity: 1 }}
-        position={"absolute"}
-        top={0}
-        bottom={0}
-        left={0}
-        right={0}
-        bg={"blackAlpha.700"}
-        transition={"all 0.3s ease"}
-        zIndex={1}
-        justifyContent={"center"}
+    <>
+      <GridItem
+        overflow={"hidden"}
+        cursor={"pointer"}
+        borderRadius={4}
+        border={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        position={"relative"}
+        aspectRatio={1 / 1}
+        onClick={onOpen}
       >
-        <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
-          <Flex>
-            <FaHeart size={20} />
-            <Text fontWeight={"bold"} ml={2}>
-              7
-            </Text>
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={"absolute"}
+          top={0}
+          bottom={0}
+          left={0}
+          right={0}
+          bg={"blackAlpha.700"}
+          transition={"all 0.3s ease"}
+          zIndex={1}
+          justifyContent={"center"}
+        >
+          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+            <Flex>
+              <FaHeart size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+            <Flex>
+              <FaComment size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
           </Flex>
-          <Flex></Flex>
         </Flex>
-      </Flex>
-    </GridItem>
+        <Image
+          src={img}
+          objectFit={"cover"}
+          alt="profile-pic"
+          w={"100%"}
+          h={"100%"}
+        />
+      </GridItem>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered={true}
+        size={{ base: "3xl", md: "5xl" }}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody bg={"black"} pb={5}>
+            <Flex
+              gap={4}
+              w={{ base: "90%", sm: "70%", md: "full" }}
+              mx={"auto"}
+            >
+              <Box
+                borderRadius={4}
+                overflow={"hidden"}
+                border={"1px solid"}
+                borderColor={"whiteAlpha.300"}
+                flex={1.5}
+              >
+                <Image src={img} alt="profile post" />
+              </Box>
+              <Flex
+                flex={1}
+                px={10}
+                display={{ base: "none", md: "flex" }}
+                flexDir={"column"}
+              >
+                <Flex alignItems={"center"} justifyContent={"space-between"}>
+                  <Flex gap={4} alignItems={"center"}>
+                    <Avatar
+                      src="/profilepic.png"
+                      size={"sm"}
+                      name="Frontend Developer"
+                    />
+                    <Text fontWeight={"bold"} fontSize={11}>
+                      fdeveloper
+                    </Text>
+                  </Flex>
+                  <Box
+                    _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+                    borderRadius={4}
+                    p={1}
+                  >
+                    <MdDelete size={20} cursor={"pointer"} />
+                  </Box>
+                </Flex>
+                <Divider my={4} bg={"gray.500"} />
+
+                <VStack
+                  w={"full"}
+                  alignItems={"start"}
+                  maxH={"350px"}
+                  overflowY={"auto"}
+                >
+                  <Comment
+                    createdAt="1d ago"
+                    username="fdeveloper"
+                    profilePic="/profilepic.png"
+                    text={"Dummy images from unsplash"}
+                  />
+                  <Comment
+                    createdAt="12h ago"
+                    username="fdeveloper"
+                    profilePic="https://bit.1y/dan-abramov"
+                    text={"Dummy images from unsplash"}
+                  />
+                  <Comment
+                    createdAt="2d ago"
+                    username="fdeveloper"
+                    profilePic="https://bit.1y/kent-c-dodds"
+                    text={"Dummy images from unsplash"}
+                  />
+                </VStack>
+              </Flex>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
