dfcd0839a24f30f1a687cf4d716dc45e37a10b03
mrduman <omer.duman@gmx.de>
2024-04-23 19:48:55 +0200

    Suggested users hook created and search functionality done

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -2,41 +2,36 @@ import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
 import SuggestedHeader from "./SuggestedHeader";
 import SuggestedUser from "./SuggestedUser";
 import { Link as RouterLink } from "react-router-dom";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 const SuggestedUsers = () => {
+  const { isLoading, suggestedUsers } = useGetSuggestedUsers();
+
+  if (isLoading) return null;
+
   return (
     <VStack py={8} gap={4}>
       <SuggestedHeader />
 
-      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-        <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
-          Suggested for you
-        </Text>
+      {suggestedUsers.length !== 0 && (
+        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+          <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+            Suggested for you
+          </Text>
 
-        <Text
-          fontSize={12}
-          fontWeight={"bold"}
-          cursor={"pointer"}
-          _hover={{ color: "gray.400" }}
-        >
-          See all
-        </Text>
-      </Flex>
+          <Text
+            fontSize={12}
+            fontWeight={"bold"}
+            cursor={"pointer"}
+            _hover={{ color: "gray.400" }}
+          >
+            See all
+          </Text>
+        </Flex>
+      )}
 
-      <SuggestedUser
-        name="Dan Abrahmov"
-        followers={1392}
-        avatar="https://bit.ly/dan-abramov"
-      />
-      <SuggestedUser
-        name="Ryan Florence"
-        followers={666}
-        avatar="https://bit.ly/ryan-florence"
-      />
-      <SuggestedUser
-        name="Christian Nwamba"
-        followers={455}
-        avatar="https://bit.ly/code-beast"
-      />
+      {suggestedUsers.map((user) => (
+        <SuggestedUser user={user} key={user.id} />
+      ))}
 
       <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
         © 2023 Built By{" "}
